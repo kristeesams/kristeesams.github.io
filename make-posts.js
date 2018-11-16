154b2cdd12d97e42ac5af81bcdf4659830f0ed90
Andy Mikulski <andy.mikulski@gmail.com>
2018-11-16 11:58:38 -0500

    Add numbers to photos for easier editing

--- make-posts.js
+++ make-posts.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const inFolder = path.resolve(__dirname, './images/');
+const inFolder = path.resolve(__dirname, './images/photos/');
 const outFolder = path.resolve(__dirname, './_posts/');
 
 let i = 0;
@@ -8,17 +8,17 @@ fs.readdir(inFolder, (err, files) => {
   files.forEach(file => {
     i++;
     if (file.endsWith('jpg') || file.endsWith('png')) {
-      fs.writeFile(path.resolve(outFolder, `./2018-11-16-Photo-${i}.md`), getTemplate(file), () => {
+      fs.writeFile(path.resolve(outFolder, `./2018-11-16-Photo-${i}.md`), getTemplate(file, i), (err) => {
         console.log(file + ' done');
       });
     }
   });
 });
 
-const getTemplate = (filename) => {
+const getTemplate = (filename, num) => {
   return `---
 layout: photo
-title: Test Photo
+title: Photo #${num}
 filename: ${filename}
 focus: center
 ---
