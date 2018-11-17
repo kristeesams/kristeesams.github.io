b252faad9782f392625686bcae3fec9f7bbda1e2
Andy Mikulski <andy.mikulski@gmail.com>
2018-11-16 23:34:30 -0500

    Update order

--- make-posts.js
+++ make-posts.js
@@ -4,10 +4,15 @@ const inFolder = path.resolve(__dirname, './images/photos/');
 const outFolder = path.resolve(__dirname, './_posts/');
 
 let i = 0;
-fs.readdir(inFolder, (err, files) => {
-  // var sorted = files.sort();
 
-  files.forEach(file => {
+function sortNumber(a, b) {
+  return parseFloat(a) - parseFloat(b);
+}
+
+fs.readdir(inFolder, (err, files) => {
+  var sorted = files.sort(sortNumber);
+
+  sorted.forEach(file => {
     i++;
     if (file.endsWith('jpg') || file.endsWith('png')) {
       fs.writeFile(path.resolve(outFolder, `./2018-11-16-Photo-${i}.md`), getTemplate(file, i), (err) => {
